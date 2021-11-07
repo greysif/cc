777089cc989abbed9ef16d71e2f9d3a73cd15477
Greysif <supaplexdesign@gmail.com>
2021-11-07 19:28:01 +0300

    new project

--- sketch-05.js
+++ sketch-05.js
@@ -1,14 +1,18 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-  };
+
+
+    context.fillStyle = 'black';
+    context.font = '400 px serif';
+    context.fillText('A', 0, 0);
 };
 
 canvasSketch(sketch, settings);
